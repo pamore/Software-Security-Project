9a7e45217ec6ee1259a23911cd42581f48b8bb7b
deepti9v <9vdeepti@gmail.com>
2016-09-26 03:32:37 -0700

    FrontEnd changes for login page

--- group3_sbs/group3_sbs/settings.py
+++ group3_sbs/group3_sbs/settings.py
@@ -145,7 +145,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'group3_sbs',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '3306',
     }
