8458dbffcdd57c79e4349e691278368db91cbdac
garrettgutierrezasu <garrettdgutierrez@gmail.com>
2016-10-07 18:52:44 -0700

    Merging with test

--- group3_sbs/group3_sbs/settings.py
+++ group3_sbs/group3_sbs/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+with open('/var/sbs_config.json') as data_file:
+    CONFIG = json.load(data_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
